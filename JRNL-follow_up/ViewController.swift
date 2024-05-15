345f24769bb7f4e6409f7dc7a56ef06cbf765760
Jin Park <jinpark1504@gmail.com>
2024-05-15 10:35:49 +0900

    Initial Commit

---
+++ JRNL-follow_up/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRNL-follow_up
+//
+//  Created by Jungjin Park on 2024-05-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
